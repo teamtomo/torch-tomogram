843c38ac208b1bde18003d40a2f41065095abc36
McHaillet <martenchaillet@gmail.com>
2025-09-12 15:42:32 +0200

    make dataclass pure Python numeric

--- src/torch_tomogram/datamodel.py
+++ src/torch_tomogram/datamodel.py
@@ -1,76 +1,42 @@
 from typing import Annotated, Optional
 from datetime import datetime
-from teamtomo_basemodel import BaseModelTeamTomo
-from pydantic import Field, AliasPath, AfterValidator
-import torch
-import torch.nn.functional as F
-from torch_affine_utils.transforms_3d import Rz, Ry, Rx, T
+from teamtomo_basemodel import BaseModelTeamTomo, ExcludedTensor
+from pydantic import Field, AliasPath
 
 
-def tensor_check(x: float | torch.Tensor) -> torch.Tensor:
-    if isinstance(x, float):
-        return torch.tensor(x)  # convert float to 0-dim tensor
-    elif isinstance(x, torch.Tensor) and x.numel() == 1:
-        return x.squeeze()  # squeeze to a 0-dim tensor
-    else:
-        raise ValueError(f"{x} is neither a float nor a single valued tensor")
-
-
-class Alignment(BaseModelTeamTomo):
+class RigidBodyAlignment(BaseModelTeamTomo):
     """Rigid body alignment parameters
 
     The alignment describes a forward projection model from the sample to
     the record exposure in the detector.
     """
     # rotation parameters
-    z_rotation_degrees: Annotated[
-        float | torch.Tensor, AfterValidator(tensor_check)
-    ] = (
-        Field(alias='tilt_axis_angle', default=torch.tensor(0.0))
-    )
-    y_rotation_degrees: Annotated[
-        float | torch.Tensor, AfterValidator(tensor_check)
-    ] = (
-        Field(alias='tilt_angle', default=torch.tensor(0.0))
-    )
-    x_rotation_degrees: Annotated[
-        float | torch.Tensor, AfterValidator(tensor_check)
-    ] = (
-        Field(alias='x_tilt', default=torch.tensor(0.0))
-    )
+    z_rotation_degrees: float = Field(alias='tilt_axis_angle', default=0.0)
+    y_rotation_degrees: float = Field(alias='tilt_angle', default=0.0)
+    x_rotation_degrees: float = Field(alias='x_tilt', default=0.0)
 
     # global image shifts
-    y_shift_px: Annotated[
-        float | torch.Tensor, AfterValidator(tensor_check)
-    ] = torch.tensor(0.0)
-    x_shift_px: Annotated[
-        float | torch.Tensor, AfterValidator(tensor_check)
-    ] = torch.tensor(0.0)
-
-    @property
-    def projection_matrix(self) -> torch.Tensor:
-        """Matrices that project points from 3D -> 2D."""
-        shifts = torch.stack((self.y_shift_px, self.x_shift_px))
-        shifts = F.pad(shifts, (1, 0), value=0)
-        r0 = Rx(self.x_rotation_degrees, zyx=True)
-        r1 = Ry(self.y_rotation_degrees, zyx=True)
-        r2 = Rz(self.z_rotation_degrees, zyx=True)
-        t3 = T(shifts)
-        return t3 @ r2 @ r1 @ r0
+    y_shift_angstrom: float = Field(default=0.0)
+    x_shift_angstrom: float = Field(default=0.0)
 
 
 class Optics(BaseModelTeamTomo):
-    raise NotImplementedError
+    defocus_u: float
+    defocus_v: float
+    # etc...
 
 
 class Exposure(BaseModelTeamTomo):
-    alignment: Alignment
-    optics_group: Optional[Optics] = None
     collection_time: Optional[datetime] = None
+    accumulated_dose: float = Field(default=0.0, gt=0.0)  # in e- per A^2
+
+    alignment: RigidBodyAlignment
+    optics: Optics
 
 
-class TiltSeries(BaseModelTeamTomo):
-    image_stack: torch.Tensor
-    tilt_image_data: list[Exposure] =(
+class ExposureSeries(BaseModelTeamTomo):
+    pixel_spacing_angstrom: float = Field(gt=0.0)
+    image_stack: ExcludedTensor
+    exposure_list: list[Exposure] =(
         Field(description="A list of metadata around tilt images")
     )
