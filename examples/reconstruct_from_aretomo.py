52010e0ba21ebd5b297514b400f2c6a947a5d342
Davide Torre <davidetorre99@gmail.com>
2025-10-24 15:23:56 +0200

    keep only tx,ty from .aln file

--- examples/reconstruct_from_aretomo.py
+++ examples/reconstruct_from_aretomo.py
@@ -12,29 +12,9 @@ TILT_STACK_PATH = Path('/Path/to/your/raw/tilt_stack.ts_ext')
 df = alnfile.read(ALN_PATH)
 
 # Get shifts for reconstruction 
-# In aretomo tx and ty already represents the shifts in the forward projection model (sample -> image) 
-# so we can either use directly those
+# In aretomo, tx and ty already represent the shifts in the forward projection model (sample -> image) 
 corrected_shifts_t_xy = df[['tx', 'ty']].to_numpy()
 corrected_shifts_t_yx = corrected_shifts_t_xy[:, ::-1].copy() #yx
-
-# or get IMOD xf components from the dataframe
-# df_to_xf(df, yx=True) returns (n_tilts, 2, 3) array
-# Each matrix is [[A22, A21, DY], [A12, A11, DX]] (ready for torch-tomogram yz) 
-xf = alnfile.df_to_xf(df, yx=True)
-m, shifts = xf[:, :, :2], xf[:, :, 2]
-# Convert IMOD's backward projection model to torch-tomogram's forward model
-# IMOD: image -> sample
-#   > the 2d matrix from the .xf file represents a 2d transform to align
-#   > the image with the tilt-axis
-# torch-tomogram: sample -> image
-#   > the shifts are applied after rotation and projection and shift the
-#   > projected sample to the image position
-#
-#  Roation matrix are orthogonal, so inversion = transposition :
-#  np.einsum('nij,nj->ni', np.linalg.inv(m), shifts) = np.einsum('nji,nj->ni', m, shifts) 
-#
-#  Negate shifts for forward projection model
-corrected_shifts_xf = -np.einsum('nji,nj->ni', m, shifts)
 
 # Load tilt stack
 tilt_stack_full = mrcfile.read(TILT_STACK_PATH)
@@ -50,8 +30,8 @@ tilt_stack /= np.std(tilt_stack, axis=(-2, -1), keepdims=True)
 tilt_series = Tomogram(
     images=tilt_stack,
     tilt_angles=df['tilt'].to_numpy(),
-    tilt_axis_angle=df['rot'].to_numpy(),  # Use single tilt axis angle
-    sample_translations=corrected_shifts_xf.copy() # or corrected_shifts_t_yx.copy()
+    tilt_axis_angle=df['rot'].to_numpy(), 
+    sample_translations=corrected_shifts_xf.copy()
 ) 
 
 tomogram = tilt_series.reconstruct_tomogram((100, 480, 320), 128)
